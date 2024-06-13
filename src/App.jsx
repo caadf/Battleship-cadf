434620d242ef50cfc8a566477d62529d13724d63
caadf <cata.adf@gmail.com>
2024-06-13 16:34:28 -0400

    comenzando a armar el tablero del juego

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Tablero from './componentes/tablero';
 
 const App = () => {
     return (
-        <h1>REACT APP</h1>
+        <>
+        <Tablero />
+       
+    </>
     )
 }
 
