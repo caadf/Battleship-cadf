46871e7f8126feddb887b7cb91af71aa9af82631
caadf <cata.adf@gmail.com>
2024-07-12 17:07:34 -0400

    cambios en el componente player

--- src/components/Computerboard.jsx
+++ src/components/Computerboard.jsx
@@ -1,2 +1,10 @@
 import React, { useEffect, useState } from "react";
 
+const inicializarTableroMaquina = () => {
+    const nuevoTableroMaquina = Array.from({ length: 10 }, () =>
+      Array(10).fill({
+        contenido: null,
+        clickeable: true,
+      })
+    );
+}
